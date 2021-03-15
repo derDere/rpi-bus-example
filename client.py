ad8bdd00a9ac197ba65d81f432c8c4678f27c0b3
Phillip Deremer <phillip@deremer.de>
2021-03-15 07:53:55 +0100

    fixed protocol done!

--- client.py
+++ client.py
@@ -51,90 +51,59 @@ class Client:
     self.queueOut = Queue()
     self._byteEvents = []
 
-  def setMode(self, mode):
+  def _setMode(self, mode):
     if mode == MODE_READ:
       IO.setup(self.gpio, IO.IN, pull_up_down=IO.PUD_DOWN)
     elif mode == MODE_WRITE:
       IO.setup(self.gpio, IO.OUT, initial=IO.HIGH)
 
-  def set(self, value):
+  def _set(self, value):
     if value:
       IO.output(self.gpio, IO.HIGH)
     else:
       IO.output(self.gpio, IO.LOW)
 
-  def get(self):
+  def _get(self):
     return IO.input(self.gpio)
 
-  def sendByte(self, byte):
-    self.set(0)
-    T.sleep(self.delay)
-    for bit in byte:
-      self.set(bit)
-      T.sleep(self.delay)
-    self.set(1)
-    T.sleep(self.delay * 2)
-
-  def readByte(self):
-    bits = []
-    timeoutCounter = 0
-    wasWaiting = False
-    while self.get():
-      wasWaiting = True
-      T.sleep(self.delay * (1/100))
-      timeoutCounter += 1
-      if timeoutCounter >= 5000:
-        timeoutCounter = 0
-        if not self.queueOut.empty():
-          return []
-    if not wasWaiting == 0:
-      return []
-    T.sleep(self.delay * 0.5)
-    for n in range(8):
-      T.sleep(self.delay)
-      bit = self.get();
-      bits.append(bit)
-    T.sleep(self.delay * 0.6)
-    return bits
-
   def _ioSync(self):
-    while self.get():
+    while self._get():
       pass
 
   def _ioRead(self):
     T.sleep(self.delay * 1.5)
     bits = []
     for n in range(8):
-      bits.append(self.get())
+      bits.append(self._get())
       T.sleep(self.delay)
     self.queueIn.put(bits)
 
   def _ioWait(self):
     if self.queueOut.empty():
       waitEnd = T.time() + (self.delay * 4)
-      while not self.get():
+      while not self._get():
         if T.time() > waitEnd:
           return True
       return False
     else:
       T.sleep(self.delay * (1 + rnd.random()))
-      if not self.get():
+      if not self._get():
         return True
       else:
         return False
 
   def _ioWrite(self):
-    self.setMode(MODE_WRITE)
-    self.set(1)
+    self._setMode(MODE_WRITE)
+    self._set(1)
     T.sleep(self.delay * 2)
     bits = self.queueOut.get()
     self.queueOut.task_done()
-    self.set(0)
+    self._set(0)
     T.sleep(self.delay)
     for bit in bits:
-      self.set(bit)
+      self._set(bit)
       T.sleep(self.delay)
-    self.set(0)
+    self._set(0)
 
   def _ioManager(self):
     while True:
@@ -144,18 +113,6 @@ class Client:
       else:
         self._ioSync()
         self._ioRead()
-    #  if not self.queueOut.empty():
-    #    T.sleep(self.delay * rnd.random())
-    #    if not self.get():
-    #      self.setMode(MODE_WRITE)
-    #      self.set(1)
-    #      T.sleep(self.delay * 3)
-    #      self.sendByte(self.queueOut.get(block=False))
-    #      self.queueOut.task_done()
-    #      self.setMode(MODE_READ)
-    #  bits = self.readByte()
-    #  if len(bits) > 0:
-    #    self.queueIn.put(bits, block=False)
 
   def _eventManager(self):
     while True:
@@ -166,7 +123,7 @@ class Client:
 
   def start(self):
     IO.setmode(IO.BCM)
-    self.setMode(MODE_READ)
+    self._setMode(MODE_READ)
     Thread(target=self._ioManager, daemon=True).start()
     Thread(target=self._eventManager, daemon=True).start()
 
@@ -193,7 +150,7 @@ def main(argv):
   if len(argv) > 1:
     bd = int(argv[1])
   c = Client(gpio, bd)
-  c.onByte(lambda b: print(chr(b), end='', flush=True) if b >= 97 and b < 123 else False)
+  c.onByte(lambda b: print(chr(b), end='', flush=True))
   c.start()
   msg = "xxx"
   while len(msg) > 0:
